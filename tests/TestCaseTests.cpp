f193f1cf01ec40cf64bd4d406117c0a08bed830a
artem.gevorkyan <artem.gevorkyan@f5334748-e0b4-5576-5d6c-fae2a85e3d10>
2011-11-18 11:13:17 +0000

    - initializers are now called before execution of a test method.

--- tests/TestCaseTests.cpp
+++ tests/TestCaseTests.cpp
@@ -7,6 +7,35 @@ namespace ut
 {
 	namespace
 	{
+		template <typename Fixture>
+		typename test_case_impl<Fixture>::methods_ptr_t vmethods()
+		{	return shared_ptr< test_case_impl<Fixture>::methods_list_t >(new test_case_impl<Fixture>::methods_list_t);	}
+
+		template <typename Fixture>
+		typename test_case_impl<Fixture>::methods_ptr_t vmethods(void (Fixture::*m1)())
+		{	return typename test_case_impl<Fixture>::methods_ptr_t(new test_case_impl<Fixture>::methods_list_t(1, m1));	}
+
+		template <typename Fixture>
+		typename test_case_impl<Fixture>::methods_ptr_t vmethods(void (Fixture::*m1)(), void (Fixture::*m2)())
+		{
+			typename test_case_impl<Fixture>::methods_ptr_t m(vmethods<Fixture>());
+
+			m->push_back(m1);
+			m->push_back(m2);
+			return m;
+		}
+
+		template <typename Fixture>
+		typename test_case_impl<Fixture>::methods_ptr_t vmethods(void (Fixture::*m1)(), void (Fixture::*m2)(), void (Fixture::*m3)())
+		{
+			typename test_case_impl<Fixture>::methods_ptr_t m(vmethods<Fixture>());
+
+			m->push_back(m1);
+			m->push_back(m2);
+			m->push_back(m3);
+			return m;
+		}
+
 		class FooTests
 		{
 		public:
@@ -49,7 +78,13 @@ namespace ut
 		template <int unique_value>
 		class UniqueValueTests
 		{
+			int _to_be_initialized;
+
 		public:
+			UniqueValueTests()
+				: _to_be_initialized(0)
+			{	}
+
 			void check_object_validity()
 			{
 				if (vvalue() != unique_value)
@@ -58,6 +93,39 @@ namespace ut
 
 			virtual int vvalue()
 			{	return unique_value;	}
+
+
+			void set_to_square()
+			{	_to_be_initialized = unique_value * unique_value;	}
+
+			void check_if_squared()
+			{
+				if (_to_be_initialized != unique_value * unique_value)
+					throw 0;
+			}
+
+			void init_1()
+			{
+				_to_be_initialized = unique_value;
+			}
+
+			void init_2()
+			{
+				++_to_be_initialized;
+				_to_be_initialized *= 5;
+			}
+
+			void init_3()
+			{
+				++_to_be_initialized;
+				_to_be_initialized *= 7;
+			}
+
+			void check_if_3_steps_initialized()
+			{
+				if (7 * (5 * (unique_value + 1) + 1) != _to_be_initialized)
+					throw 0;
+			}
 		};
 
 
@@ -65,21 +133,23 @@ namespace ut
 			test(TestCaseImplementationsUpcastableToTestCase)
 			{
 				// INIT / ACT
-				test_case_impl<FooTests> ft(&FooTests::smoke, "x");
-				test_case_impl<BarTests> bt(&BarTests::smoke, "y");
+				test_case_impl<FooTests> ft(&FooTests::smoke, "x", vmethods<FooTests>(), vmethods<FooTests>());
+				test_case_impl<BarTests> bt(&BarTests::smoke, "y", vmethods<BarTests>(), vmethods<BarTests>());
 
 				// ACT / ASSERT (must compile)
 				test_case &tc_ft = ft;
 				test_case &tc_bt = bt;
+
+				tc_ft, tc_bt;
 			}
 
 
 			test(TestCaseNameIsSetInCtor)
 			{
 				// INIT / ACT
-				test_case_impl<FooTests> t1(&FooTests::smoke, "x");
-				test_case_impl<BarTests> t2(&BarTests::smoke, "Smoke");
-				test_case_impl<BarTests> t3(&BarTests::smoke, "z z z");
+				test_case_impl<FooTests> t1(&FooTests::smoke, "x", vmethods<FooTests>(), vmethods<FooTests>());
+				test_case_impl<BarTests> t2(&BarTests::smoke, "Smoke", vmethods<BarTests>(), vmethods<BarTests>());
+				test_case_impl<BarTests> t3(&BarTests::smoke, "z z z", vmethods<BarTests>(), vmethods<BarTests>());
 				test_case &tc_t1 = t1;
 				test_case &tc_t2 = t2;
 				test_case &tc_t3 = t3;
@@ -94,8 +164,8 @@ namespace ut
 			test(FailedExecutionSetsExecutionResult)
 			{
 				// INIT
-				test_case_impl<FooTests> tf(&FooTests::always_fails, "");
-				test_case_impl<BarTests> tb(&BarTests::always_fails, "");
+				test_case_impl<FooTests> tf(&FooTests::always_fails, "", vmethods<FooTests>(), vmethods<FooTests>());
+				test_case_impl<BarTests> tb(&BarTests::always_fails, "", vmethods<BarTests>(), vmethods<BarTests>());
 
 				// ACT
 				test_result rf(tf.execute());
@@ -110,8 +180,8 @@ namespace ut
 			test(PassedExecutionSetsExecutionResult)
 			{
 				// INIT
-				test_case_impl<FooTests> tf(&FooTests::always_passes, "");
-				test_case_impl<BarTests> tb(&BarTests::always_passes, "");
+				test_case_impl<FooTests> tf(&FooTests::always_passes, "", vmethods<FooTests>(), vmethods<FooTests>());
+				test_case_impl<BarTests> tb(&BarTests::always_passes, "", vmethods<BarTests>(), vmethods<BarTests>());
 
 				// ACT
 				test_result rf(tf.execute());
@@ -126,8 +196,10 @@ namespace ut
 			test(TestExecutionIsDoneForAValidFixtureObject)
 			{
 				// INIT
-				test_case_impl< UniqueValueTests<1> > t1(&UniqueValueTests<1>::check_object_validity, "");
-				test_case_impl< UniqueValueTests<101> > t2(&UniqueValueTests<101>::check_object_validity, "");
+				test_case_impl< UniqueValueTests<1> > t1(&UniqueValueTests<1>::check_object_validity, "",
+					vmethods< UniqueValueTests<1> >(), vmethods< UniqueValueTests<1> >());
+				test_case_impl< UniqueValueTests<101> > t2(&UniqueValueTests<101>::check_object_validity, "",
+					vmethods< UniqueValueTests<101> >(), vmethods< UniqueValueTests<101> >());
 
 				// ACT
 				test_result r1(t1.execute());
@@ -142,9 +214,10 @@ namespace ut
 			test(ReturnFixtureName)
 			{
 				// INIT
-				test_case_impl< UniqueValueTests<1> > t1(&UniqueValueTests<1>::check_object_validity, "");
-				test_case_impl<FooTests> t2(&FooTests::smoke, "");
-				test_case_impl<BarTests> t3(&BarTests::smoke, "");
+				test_case_impl< UniqueValueTests<1> > t1(&UniqueValueTests<1>::check_object_validity, "",
+					vmethods< UniqueValueTests<1> >(), vmethods< UniqueValueTests<1> >());
+				test_case_impl<FooTests> t2(&FooTests::smoke, "", vmethods<FooTests>(), vmethods<FooTests>());
+				test_case_impl<BarTests> t3(&BarTests::smoke, "", vmethods<BarTests>(), vmethods<BarTests>());
 				test_case *tests[] = {	&t1, &t2, &t3,	};
 
 				// ACT / ASSERT
@@ -157,9 +230,43 @@ namespace ut
 			test(InitializersAreCalledBeforeTestExecution)
 			{
 				// INIT
+				test_case_impl< UniqueValueTests<3> > t1(&UniqueValueTests<3>::check_if_squared, "",
+					vmethods(&UniqueValueTests<3>::set_to_square), vmethods< UniqueValueTests<3> >());
+				test_case_impl< UniqueValueTests<12> > t2(&UniqueValueTests<12>::check_if_squared, "",
+					vmethods(&UniqueValueTests<12>::set_to_square), vmethods< UniqueValueTests<12> >());
 
 				// ACT
+				test_result r1(t1.execute());
+				test_result r2(t2.execute());
+
 				// ASSERT
+				is_true(r1.passed);
+				is_true(r1.passed);
+			}
+
+
+			test(InitializersAreCalledInSequence)
+			{
+				// INIT
+				test_case_impl< UniqueValueTests<3> > t1(&UniqueValueTests<3>::check_if_3_steps_initialized, "",
+					vmethods(&UniqueValueTests<3>::init_3, &UniqueValueTests<3>::init_2, &UniqueValueTests<3>::init_1),
+					vmethods< UniqueValueTests<3> >());
+				test_case_impl< UniqueValueTests<11> > t2(&UniqueValueTests<11>::check_if_3_steps_initialized, "",
+					vmethods(&UniqueValueTests<11>::init_3, &UniqueValueTests<11>::init_1, &UniqueValueTests<11>::init_2),
+					vmethods< UniqueValueTests<11> >());
+				test_case_impl< UniqueValueTests<13> > t3(&UniqueValueTests<13>::check_if_3_steps_initialized, "",
+					vmethods(&UniqueValueTests<13>::init_1, &UniqueValueTests<13>::init_2, &UniqueValueTests<13>::init_3),
+					vmethods< UniqueValueTests<13> >());
+
+				// ACT
+				test_result r1(t1.execute());
+				test_result r2(t2.execute());
+				test_result r3(t3.execute());
+
+				// ASSERT
+				is_false(r1.passed);
+				is_false(r2.passed);
+				is_true(r3.passed);
 			}
 
 		end_test_suite
