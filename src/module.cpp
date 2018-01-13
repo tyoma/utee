14340383461a183c157db76c5ee9f5744e8e2740
Artem G <artem.gevorkyan@gmail.com>
2018-01-13 14:08:51 -0700

    Made working on MacOS

--- src/module.cpp
+++ src/module.cpp
@@ -5,6 +5,8 @@
 #include <windows.h>
 #elif __unix__
 #include <dlfcn.h>
+#else
+#include <dlfcn.h>
 #endif
 
 using namespace std;
@@ -15,6 +17,8 @@ namespace ut
 #ifdef _WIN32
 		: _module(::LoadLibraryA(path), &::FreeLibrary)
 #elif __unix__
+		: _module(::dlopen(path, RTLD_NOW), &::dlclose)
+#else
 		: _module(::dlopen(path, RTLD_NOW), &::dlclose)
 #endif
 	{
@@ -27,5 +31,7 @@ namespace ut
 	{  return ::GetProcAddress(static_cast<HMODULE>(_module.get()), name); }
 #elif __unix__
 	{  return ::dlsym(_module.get(), name); }
+#else
+    {  return ::dlsym(_module.get(), name); }
 #endif
 }
