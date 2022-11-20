34eadc9d3016f612bb410ae36920ac047697f3c0
Artem G <artem.gevorkyan@gmail.com>
2022-11-20 13:15:23 -0700

    MSVC debugger configuration added to utee-test targets

--- make/build.props/utee-test.cmake
+++ make/build.props/utee-test.cmake
@@ -1,4 +1,9 @@
 function(add_utee_test test_target)
 	target_link_libraries(${test_target} utee)
 	add_test(NAME ${test_target} COMMAND $<TARGET_FILE:utee-runner> $<TARGET_FILE:${test_target}> WORKING_DIRECTORY $<TARGET_FILE_DIR:${test_target}>)
+
+	set_target_properties(${test_target} PROPERTIES
+		VS_DEBUGGER_COMMAND $<TARGET_FILE:utee-runner>
+		VS_DEBUGGER_COMMAND_ARGUMENTS $<TARGET_FILE:${test_target}>
+		VS_DEBUGGER_WORKING_DIRECTORY $<TARGET_FILE_DIR:${test_target}>)
 endfunction()
